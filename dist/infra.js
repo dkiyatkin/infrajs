d7b993fe07ad4f57f511d4375d349ca8f25e77bf
Dmitriy Kiyatkin <d.kiyatkin@itlf.ru>
2012-08-08 21:28:45 +0400

    title and meta tags

--- dist/infra.js
+++ dist/infra.js
@@ -1739,22 +1739,52 @@ Infra.ext(function() {
 Infra.ext(function() { // Расширение позволяющие сборке работать со ссылками
 	var infra = this;
 	infra.set.title = function(titleObj) {
+		var updateMeta = function(metatags, attr, head) {
+			var update = false;
+			var cnt; for (cnt = 0; cnt < metatags.length; cnt++) {
+				var name = metatags[cnt].getAttribute("name");
+				if (name) {
+					name = name.toLowerCase();
+					if (name == attr) {
+						update = true;
+						metatags[cnt].setAttribute("content", infra.meta[attr]);
+					}
+			  	}
+			}
+			if (!update) { // создаем новый
+				var meta = document.createElement('meta');
+				meta.setAttribute("name", attr);
+				meta.setAttribute("content", infra.meta[attr]);
+				head.appendChild(meta);
+			}
+		};
 		var not_found = titleObj['404'];
 		var main = titleObj.main;
 		var sub = titleObj.sub;
 		infra.on('start', function() {
+			infra.meta = {};
 			infra.status_code = 200;
+			infra.title = false;
 		});
 		infra.on('end', function() {
-			if (infra.status_code == 404) {
-				infra.title = not_found;
-			} else if (infra.state === '/') {
-				infra.title = main;
-			} else {
-				infra.title = infra.state.replace(/\/+$/, '').replace(/^\/+/, '').split('/').reverse().join(' / ') + sub;
+			if (!infra.title) { // если до этого не определился вручную
+				if (infra.status_code == 404) {
+					infra.title = not_found;
+				} else if (infra.state === '/') {
+					infra.title = main;
+				} else {
+					infra.title = infra.state.replace(/\/+$/, '').replace(/^\/+/, '').split('/').reverse().join(' / ') + sub;
+				}
+				infra.last_status_code = infra.status_code;
 			}
-			infra.last_status_code = infra.status_code;
-			document.title = infra.title;
+			if (typeof(window) != 'undefined') { // на сервере title ставится из infra.title
+				document.title = infra.title;
+				// установить метатэги
+				var metatags = document.getElementsByTagName("meta");
+				var head = document.getElementsByTagName('head')[0];
+				if (infra.meta.keywords) { updateMeta(metatags, 'keywords', head); }
+				if (infra.meta.description) { updateMeta(metatags, 'description', head); }
+			}
 		});
 	};
 });
@@ -1769,7 +1799,7 @@ Infra.ext(function() { // Расширение позволяющие сборк
 		infra.once('start', function() {
 			var i; for (i = infra.layers.length; --i >= 0;) {
 				if (infra.layers[i].jsontpl) {
-					infra.layers[i].json = infra.parsetpl(infra.layers[i].jsontpl, infra);
+					infra.layers[i].json = infra.parsetpl(infra.layers[i].jsontpl, infra.layers[i]);
 				}
 			}
 		});
--- src/props/proptpl.js
+++ src/props/proptpl.js
@@ -9,7 +9,7 @@
 		infra.once('start', function() {
 			var i; for (i = infra.layers.length; --i >= 0;) {
 				if (infra.layers[i].jsontpl) {
-					infra.layers[i].json = infra.parsetpl(infra.layers[i].jsontpl, infra);
+					infra.layers[i].json = infra.parsetpl(infra.layers[i].jsontpl, infra.layers[i]);
 				}
 			}
 		});
